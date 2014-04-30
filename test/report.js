7d7006825be8c48f6da71b542aee422bbbf4d2ce
Alex Cline <alex.cline@gmail.com>
2014-04-30 14:52:31 -0400

    Added ability to pull XML report, since JSON doesn't include proper plugin output formatting.

--- test/report.js
+++ test/report.js
@@ -26,9 +26,22 @@ describe("Report", function(){
       nessus.report2Vulnerabilities(
         'effd7284-a82a-c2c9-5471-1acb68a26e5ad7723195da5110f7',
         function(err, report){
-          console.log(report);
           assert.ifError(err);
           assert(report);
+          done();
+      });
+    });
+  });
+
+  describe("#report2HostsPlugin", function(){
+    it("should return a valid report2HostsPlugin object", function(done){
+      nessus.report2HostsPlugin(
+        '8f2bd681-c7da-8536-e71c-3db783dae676db9e0d28d5c7b970', // reportId
+        '1', // severity
+        '65821', // pluginId
+        function(err, plugin){
+          assert.ifError(err);
+          assert(plugin);
           done();
       });
     });
